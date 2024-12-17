628b551151894e5a4e9f2fbbb20077bc24bb37f3
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2024-12-17 15:07:55 -0600

    better naming

--- repositories/pg.go
+++ repositories/pg.go
@@ -15,9 +15,9 @@ func NewPGRepository(pool interfaces.Pool) *PGRepository {
 	return &PGRepository{pool: pool}
 }
 
-// buildQuery executes sql query with variable number of arguments.
+// runQuery executes sql query with variable number of arguments.
 // sql should return (string, boolean) otherwise an error will return.
-func (pg *PGRepository) buildQuery(ctx context.Context, query string, args ...interface{}) (string, bool, error) {
+func (pg *PGRepository) runQuery(ctx context.Context, query string, args ...interface{}) (string, bool, error) {
 	var (
 		id string
 		ok bool
@@ -25,14 +25,14 @@ func (pg *PGRepository) buildQuery(ctx context.Context, query string, args ...in
 
 	err := pg.pool.QueryRow(ctx, query, args...).Scan(&id, &ok)
 	if err != nil {
-		return "", false, fmt.Errorf("failed to create entity: %w", err)
+		return "", false, fmt.Errorf("failed to run query: %w", err)
 	}
 
 	return id, ok, nil
 }
 
-// CreateUser creates a new user using the generic buildQuery function
+// CreateUser creates a new user using the generic runQuery function
 func (pg *PGRepository) CreateUser(ctx context.Context, user domain.User) (string, bool, error) {
 	query := `SELECT * FROM create_user($1, $2);`
-	return pg.buildQuery(ctx, query, user.Email, user.Password)
+	return pg.runQuery(ctx, query, user.Email, user.Password)
 }
