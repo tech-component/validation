db38dd85370865e12d422052cdf51149903e8385
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2024-12-17 15:04:16 -0600

    make query more generic

--- repositories/pg.go
+++ repositories/pg.go
@@ -3,7 +3,6 @@ package repositories
 import (
 	"context"
 	"fmt"
-
 	"github.com/tech-component/validation/domain"
 	"github.com/tech-component/validation/interfaces"
 )
@@ -16,19 +15,24 @@ func NewPGRepository(pool interfaces.Pool) *PGRepository {
 	return &PGRepository{pool: pool}
 }
 
-func (pg *PGRepository) CreateUser(ctx context.Context, user domain.User) (string, bool, error) {
+// buildQuery executes sql query with variable number of arguments.
+// sql should return (string, boolean) otherwise an error will return.
+func (pg *PGRepository) buildQuery(ctx context.Context, query string, args ...interface{}) (string, bool, error) {
 	var (
 		id string
 		ok bool
 	)
 
-	query := `SELECT * FROM create_user($1, $2);`
-
-	// Execute the query
-	err := pg.pool.QueryRow(ctx, query, user.Email, user.Password).Scan(&id, &ok)
+	err := pg.pool.QueryRow(ctx, query, args...).Scan(&id, &ok)
 	if err != nil {
-		return "", false, fmt.Errorf("failed to create user: %w", err)
+		return "", false, fmt.Errorf("failed to create entity: %w", err)
 	}
 
 	return id, ok, nil
+}
+
+// CreateUser creates a new user using the generic buildQuery function
+func (pg *PGRepository) CreateUser(ctx context.Context, user domain.User) (string, bool, error) {
+	query := `SELECT * FROM create_user($1, $2);`
+	return pg.buildQuery(ctx, query, user.Email, user.Password)
 }
